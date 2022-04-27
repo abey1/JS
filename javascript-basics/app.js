04daae0591855c9aebf54b29c8d69aebfa94eb45
abey1 <bruckabey@gmail.com>
2022-04-27 17:44:14 +0300

    javascript basics just started

---
+++ javascript-basics/app.js
@@ -0,0 +1,10 @@
+// document.querySelectorAll(".btn").forEach((item, index) => {
+//   index % 2 === 0 &&
+//     item.addEventListener("click", () => {
+//       alert("this is good i just added this!");
+//     });
+// });
+
+document.write(2 + 2);
+alert("hello people!");
+console.log("hello world");
